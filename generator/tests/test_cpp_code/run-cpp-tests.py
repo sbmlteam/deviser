e21934f8c117ade13ee351d5cfc7495df9ef9b5b
Sarah Keating <skeating@caltech.edu>
2015-06-20 10:24:48 +0100

    use either new or old parser

--- generator/tests/test_cpp_code/run-cpp-tests.py
+++ generator/tests/test_cpp_code/run-cpp-tests.py
@@ -4,10 +4,23 @@ import os
 
 from parseXML import createPackageFromXml
 from code_files import CppFiles
+from parseXML import ParseXML
+
+use_new = False
 
 
 def generate_cpp_header(filename, num):
     ob = createPackageFromXml.parse_deviser_xml(filename)
+    working_class = ob['sbmlElements'][num]
+    os.chdir('./temp')
+    all_files = CppFiles.CppFiles(working_class)
+    all_files.write_files()
+    os.chdir('../.')
+
+
+def generate_new_cpp_header(filename, num):
+    parser = ParseXML.ParseXML(filename)
+    ob = parser.parse_deviser_xml()
     working_class = ob['sbmlElements'][num]
     os.chdir('./temp')
     all_files = CppFiles.CppFiles(working_class)
@@ -38,7 +51,10 @@ def compare_files(infile, outfile):
 def run_test(name, num, class_name, test_case, list_of):
     filename = '.\\test_xml_files\\{}.xml'.format(name)
     print('Testing {}:{} {}'.format(name, class_name, test_case))
-    generate_cpp_header(filename, num)
+    if use_new:
+        generate_new_cpp_header(filename, num)
+    else:
+        generate_cpp_header(filename, num)
     correct_file = '.\\test-code\\{}.h'.format(class_name)
     temp_file = '.\\temp\\{}.h'.format(class_name)
     fail = compare_files(correct_file, temp_file)
@@ -147,40 +163,6 @@ def main():
     if fail > 0:
         print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
         print('Check {} fails'.format(fail))
-    # filename = 'C:\Development\git\deviser\samples\qual.xml'
-    #
-    # generate_cpp_header(filename, 3)
-    #
-    # my_file = '.\\test-code\\Output.h'
-    # new_file = '.\\temp\\Output.h'
-    #
-    # compare_files(my_file, new_file)
-    #
-    # my_file = '.\\test-code\\ListOfOutputs.h'
-    # new_file = 'ListOfOutputs.h'
-    #
-    # compare_files(my_file, new_file)
-    #
-    # generate_cpp_header(filename, 1)
-    #
-    # my_file = '.\\test-code\\Transition.h'
-    # new_file = 'Transition.h'
-    #
-    # compare_files(my_file, new_file)
-    #
-    # my_file = '.\\test-code\\ListOfTransitions.h'
-    # new_file = 'ListOfTransitions.h'
-    #
-    # compare_files(my_file, new_file)
-    #
-    # filename = 'C:\Users\Sarah\Dropbox\sbmlteam\deviser\specs2\\spatial.xml'
-    #
-    # generate_cpp_header(filename, 12)
-    #
-    # my_file = '.\\test-code\\AnalyticVolume.h'
-    # new_file = 'AnalyticVolume.h'
-    #
-    # compare_files(my_file, new_file)
 
 
 if __name__ == '__main__':
