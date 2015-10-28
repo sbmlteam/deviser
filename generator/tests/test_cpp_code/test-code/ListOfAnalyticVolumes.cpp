624588295641c2cf0ca00c7b0de161964ce041db
Sarah Keating <skeating@caltech.edu>
2015-10-28 12:42:40 +0000

    updated the copyright year on licence added - fixed all tests

--- generator/tests/test_cpp_code/test-code/ListOfAnalyticVolumes.cpp
+++ generator/tests/test_cpp_code/test-code/ListOfAnalyticVolumes.cpp
@@ -7,7 +7,7 @@
  * This file is part of libSBML. Please visit http://sbml.org for more
  * information about SBML, and the latest version of libSBML.
  *
- * Copyright (C) 2013-2014 jointly by the following organizations:
+ * Copyright (C) 2013-2015 jointly by the following organizations:
  * 1. California Institute of Technology, Pasadena, CA, USA
  * 2. EMBL European Bioinformatics Institute (EMBL-EBI), Hinxton, UK
  * 3. University of Heidelberg, Heidelberg, Germany
@@ -351,15 +351,15 @@ ListOfAnalyticVolumes::createObject(XMLInputStream& stream)
 {
   const std::string& name = stream.peek().getName();
   SBase* object = NULL;
+  SPATIAL_CREATE_NS(spatialns, getSBMLNamespaces());
 
   if (name == "analyticVolume")
   {
-    SPATIAL_CREATE_NS(spatialns, getSBMLNamespaces());
     object = new AnalyticVolume(spatialns);
     appendAndOwn(object);
-    delete spatialns;
   }
 
+  delete spatialns;
   return object;
 }
 
