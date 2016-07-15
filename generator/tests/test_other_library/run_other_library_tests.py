6e4c84ef46751e834e5fbae030446b073c076d26
Sarah Keating <skeating@caltech.edu>
2016-07-15 15:51:16 +0100

    and another

--- generator/tests/test_other_library/run_other_library_tests.py
+++ generator/tests/test_other_library/run_other_library_tests.py
@@ -448,6 +448,12 @@ def main():
 
     name = 'combine-archive'
     class_name = 'libcombine'
+    test_case = 'swig dir'
+    binding = 'swig'
+    fail += test_bindings(name, class_name, test_case, binding, 'combine')
+
+    name = 'combine-archive'
+    class_name = 'libcombine'
     test_case = 'cmake'
     binding = 'cmake'
     fail += test_cmake(name, class_name, test_case, binding, 'combine')
