3d67a5120bd01947775d51c5d74276a600fc3db4
Matthew Gillman <m.gillman@ucl.ac.uk>
2020-12-08 16:42:48 +0000

    Added tests for lower_first().

--- generator/tests/test_strfunc_code/run_strfunc_tests.py
+++ generator/tests/test_strfunc_code/run_strfunc_tests.py
@@ -33,13 +33,22 @@ def main():
     # run the individual tests
     fail = 0
 
+    # upper_first() tests
     fail += run_strfunc_test(sf.upper_first, 'cat', 'Cat')
     # fail += run_strfunc_test(sf.upper_first, 'cat', 'CAT')  # Failure test
     data = {'cat': 'Cat', 'csgsomething': 'CSGsomething', 'csgcat': 'CSGcat',
             'cscat': 'Cscat', 'csgeometry': 'CSGeometry',
             'csGeometry': 'CSGeometry', 'a': 'A'}
+    data2 = {}  # Exact opposite of the data dictionary. Used in next test!
     for (input, expected) in data.items():
         fail += run_strfunc_test(sf.upper_first, input, expected)
+        data2[expected] = input
+
+    # lower_first() tests - it's virtually the exact opposite of upper_first()
+    for (input, expected) in data2.items():
+        fail += run_strfunc_test(sf.lower_first, input, expected)
+
+
 
     # A varargs example
     fail += run_strfunc_test(sf.wrap_token, 'fred',
@@ -51,3 +60,41 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+    """
+    remaining functions to test:
+    
+lower_first
+get_indent
+abbrev_name
+abbrev_lo_name
+list_of_name
+lower_list_of_name_no_prefix
+cap_list_of_name
+cap_list_of_name_no_prefix
+plural_no_prefix
+plural
+singular
+remove_prefix
+get_indefinite
+standard_element_name
+get_library_suffix
+wrap_token
+wrap_type
+wrap_section
+make_class
+wrap_enum
+get_sid_refs
+get_element_name
+get_element_name_no_prefix
+replace_digits
+replace_underscore
+remove_spaces
+remove_hyphens
+texify
+compare_no_case
+get_class_from_plugin
+prefix_name
+prefix_classes
+is_camel_case
+"""
