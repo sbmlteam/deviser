9d328a5b30ce536144336bf2e5479ec5002c1eac
Matthew Gillman <m.gillman@ucl.ac.uk>
2020-12-15 16:28:16 +0000

    Started documenting test_functions.py.

--- generator/tests/test_functions.py
+++ generator/tests/test_functions.py
@@ -15,24 +15,37 @@ function_table = {'binding': 'run_bindings_tests',
                   'tex': 'run_tex_tests'}
 
 
-# set up
 def set_path_to_tests(dirname):
+    """
+    Set up; set global value of the path to the tests.
+
+    :param dirname: the directory containing the tests.
+    :return: nothing
+    """
     global path_to_tests
     path_to_tests = dirname
     remove_temp()
     set_running_tests()
 
 
-# set running tests variable
-# needs to be separate as the global run test function uses it
 def set_running_tests():
+    """
+    Set running tests variable.
+    Needs to be separate as the global run test function uses it
+    """
     gv.running_tests = True
     gv.code_returned = gv.return_codes['success']
 
 
-# get the xml filename
-# and print a banner defining the test
 def set_up_test(name, class_name, test_case=''):
+    """
+    Get the xml filename, and print a banner defining the test.
+
+    :param name: stub of XML filename, e.g. 'dyn' for filename 'dyn.xml'
+    :param class_name: test class, e.g.
+    :param test_case: brief test description, to output on screen
+    :return: path to the required XML file.
+    """
     set_running_tests()
     print('====================================================')
     print('Testing {0}:{1} {2}'.format(name, class_name, test_case))
@@ -42,16 +55,29 @@ def set_up_test(name, class_name, test_case=''):
     return filename
 
 
-# reads file containing expected sbml model and returns it as string
 def read_file(path):
+    """
+    Reads file containing expected sbml model and returns it as string.
+
+    :param path: location of file to read
+    :returns: contents of the file, as a string.
+    """
     filein = open(path, 'r')
     contents = filein.read()
     filein.close()
     return contents
 
 
-# do a string comparison of the contents of two file
 def compare_files(infile, outfile, fails, not_tested):
+    """
+    Do a string comparison of the contents of two files.
+
+    :param infile: reference file
+    :param outfile: file generated during tests
+    :param fails: list of failure cases
+    :param not_tested: list of untested cases
+    :returns: 0 on success, or file not present; 1 on failure. TODO if file is not present, isn't that a failure?
+    """
     ret = 0
     if not os.path.isfile(infile):
         # we have not added a file to compare to
@@ -74,6 +100,15 @@ def compare_files(infile, outfile, fails, not_tested):
 
 
 def compare_return_codes(name, flag, expected_return, fails):
+    """
+    Compare return code from a test with that expected.
+
+    :param name:
+    :param flag:
+    :param expected_return: expected return code from a test
+    :param fails: list of failure cases
+    :return: 0 on success, 1 on failure.
+    """
     ret = 0
     this_return = gv.code_returned
     expected = gv.get_return_code(expected_return)
@@ -137,6 +172,9 @@ def report(name, fail, fails, not_tested):
 
 
 def remove_temp():
+    """
+    Create temp directory, or replace it if it exists already.
+    """
     if not os.path.isdir('temp'):
         os.mkdir('temp')
     else:
@@ -145,6 +183,12 @@ def remove_temp():
 
 
 def create_dir(newdir):
+    """
+    Create a new directory, or replace it if it exists already.
+
+    :param newdir: path to the new directory (or the one to be replaced)
+    :return: nothing
+    """
     if not os.path.isdir(newdir):
         os.mkdir(newdir)
     else:
