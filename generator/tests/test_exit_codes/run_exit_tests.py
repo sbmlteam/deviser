31103f50b9bc6116f5a47eb6dc4fc9fe67bbd543
Matthew Gillman <m.gillman@ucl.ac.uk>
2020-12-15 15:41:58 +0000

    Tidyin gup a bit and adding function docstring.

--- generator/tests/test_exit_codes/run_exit_tests.py
+++ generator/tests/test_exit_codes/run_exit_tests.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
 import os
-
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../../')
-import test_functions
-
-from util import global_variables
 import deviser
+import test_functions
+from util import global_variables as gv
+
 
 ##############################################################################
 # Set up variables
@@ -28,20 +27,28 @@ def generate_deviser(args):
 
 
 def run_deviser_test(name, flag, expected_return):
-    error = global_variables.get_return_code(expected_return)
+    """
+    General function for running Deviser tests.
+
+    :param name: general name of test
+    :param flag: a command-line flag
+    :param expected_return: what we expect the test to return.
+    :return: 0 on success, 1 on failure
+    """
+    error = gv.get_return_code(expected_return)
     filename = test_functions.set_up_test(name, flag, error)
     args = []
-    if flag == '-g'or flag == '-l':
+    if flag in ['-g', '-l', 'wrong']:  # == '-g'or flag == '-l':
         args.append('deviser')
         args.append(flag)
         args.append(filename)
     elif flag == 'missing':
         args.append('deviser')
         args.append(filename)
-    elif flag == 'wrong':
-        args.append('deviser')
-        args.append(flag)
-        args.append(filename)
+    #elif flag == 'wrong':
+    #    args.append('deviser')
+    #    args.append(flag)
+    #    args.append(filename)
 
     generate_deviser(args)
     fail = test_functions.compare_return_codes(name, flag, expected_return,
@@ -56,7 +63,7 @@ def run_deviser_test(name, flag, expected_return):
 
 def main():
 
-    # set up the enivornment
+    # Set up the environment.
     this_dir = os.path.dirname(os.path.abspath(__file__))
 
     (path_to_tests, other) = os.path.split(this_dir)
@@ -64,41 +71,31 @@ def main():
 
     fail = 0
 
-    # run the individual tests
+    # Run the individual tests.
     fail += run_deviser_test('non-existent', '-g',
-                             global_variables.
-                             return_codes['failed to read file'])
+                             gv.return_codes['failed to read file'])
     fail += run_deviser_test('test_child', '-g',
-                             global_variables.
-                             return_codes['success'])
+                             gv.return_codes['success'])
     fail += run_deviser_test('test_child', 'missing',
-                             global_variables.
-                             return_codes['missing function argument'])
+                             gv.return_codes['missing function argument'])
     fail += run_deviser_test('test_child', 'wrong',
-                             global_variables.
-                             return_codes['invalid function arguments'])
+                             gv.return_codes['invalid function arguments'])
     # TODO sort for latest python
     # fail += run_deviser_test('test_child', '-l',
-    #                          global_variables.
+    #                          gv.
     #                          return_codes['success'])
     fail += run_deviser_test('invalid', '-g',
-                             global_variables.
-                             return_codes['parsing error'])
+                             gv.return_codes['parsing error'])
     fail += run_deviser_test('invalid', '-l',
-                             global_variables.
-                             return_codes['parsing error'])
+                             gv.return_codes['parsing error'])
     fail += run_deviser_test('unknown_type', '-g',
-                             global_variables.
-                             return_codes['unknown type used'])
+                             gv.return_codes['unknown type used'])
     fail += run_deviser_test('unknown_error', '-g',
-                             global_variables.
-                             return_codes['missing required information'])
+                             gv.return_codes['missing required information'])
     fail += run_deviser_test('bad_lo_element', '-g',
-                             global_variables.
-                             return_codes['unknown type used'])
+                             gv.return_codes['unknown type used'])
     fail += run_deviser_test('bad_concretes', '-g',
-                             global_variables.
-                             return_codes['missing required information'])
+                             gv.return_codes['missing required information'])
 
 
     test_functions.report('EXIT CODES', fail, fails, not_tested)
