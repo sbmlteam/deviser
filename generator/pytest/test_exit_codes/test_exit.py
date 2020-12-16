ddd95275ec318fa68d05c11f79e73f7fc7a72e08
Matthew Gillman <m.gillman@ucl.ac.uk>
2020-12-16 15:26:57 +0000

    Initial commit of pytest code.

---
+++ generator/pytest/test_exit_codes/test_exit.py
@@ -0,0 +1,56 @@
+# Trying out pytest with Deviser generator tests
+# Matthew Gillman, UCL, 16th December 2020
+import os
+import sys
+import pytest
+# If we can restructure it ok, we might not need this...
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../../')
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../../tests')
+import deviser
+import test_functions
+from util import global_variables as gv
+
+#@pytest.mark.parametrize("number", [1, 2, 3, 0, 42])
+@pytest.mark.parametrize("name, flag, expected_return", [
+    ('non-existent', '-g', 'failed to read file'),
+    ('test_child', '-g', 'success'),
+    ('test_child', 'missing', 'missing function argument'),
+    ('test_child', 'wrong', 'invalid function arguments'),
+    # ('test_child', '-l', 'success'),
+    ('invalid', '-g', 'parsing error'),
+    ('invalid', '-l', 'parsing error'),
+    ('unknown_type', '-g', 'unknown type used'),
+    ('unknown_error', '-g', 'missing required information'),
+    ('bad_lo_element', '-g', 'unknown type used'),
+    ('bad_concretes', '-g', 'missing required information'),
+])
+def test_deviser(name, flag, expected_return):
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    (path_to_tests, other) = os.path.split(this_dir)
+    test_functions.set_path_to_tests(path_to_tests)
+
+    expected_return = gv.return_codes[expected_return]
+    error = gv.get_return_code(expected_return)
+    filename = test_functions.set_up_test(name, flag, error)
+    args = []
+    if flag in ['-g', '-l', 'wrong']:
+        args.append('deviser')
+        args.append(flag)
+        args.append(filename)
+    elif flag == 'missing':
+        args.append('deviser')
+        args.append(filename)
+
+    deviser.main(args)
+    #fail = test_functions.compare_return_codes(name, flag, expected_return,
+    #                                           fails)
+    print('')
+    #return fail
+
+#ef func(x):
+#    return x + 1
+
+#def test_answer():
+#    assert func(3) == 4
+
