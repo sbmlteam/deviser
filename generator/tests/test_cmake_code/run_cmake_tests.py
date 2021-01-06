d17e0558402332156f2f3df970b422308baccf7e
Matthew Gillman <m.gillman@ucl.ac.uk>
2021-01-06 10:45:04 +0000

    Finished most of the basic documentation for run_cmake_tests.py.

--- generator/tests/test_cmake_code/run_cmake_tests.py
+++ generator/tests/test_cmake_code/run_cmake_tests.py
@@ -26,7 +26,7 @@ def generate_cmake_files(filename):
     """
     Generic set-up code used by the test wrapper function.
     Parse XML file and use the big dictionary structure obtained to
-    generate the CMake package files.
+    generate the CMake `package` files.
 
     :param filename: the XML file to parse
     :return: nothing
@@ -42,6 +42,14 @@ def generate_cmake_files(filename):
 
 
 def generate_cmake_register_files(filename):
+    """
+    Generic set-up code used by the test wrapper function.
+    Parse XML file and use the big dictionary structure obtained to
+    generate the CMake `register` files.
+
+    :param filename: the XML file to parse
+    :return: nothing
+    """
     parser = ParseXML.ParseXML(filename)
     ob = parser.parse_deviser_xml()
     os.chdir(os.path.normpath('./temp'))
@@ -49,7 +57,17 @@ def generate_cmake_register_files(filename):
     all_files.write_register_files()
     os.chdir(os.path.normpath('../.'))
 
+
 def generate_cmake_example_files(filename, name):
+    """
+    Generic set-up code used by the test wrapper function.
+    Parse XML file and use the big dictionary structure obtained to
+    generate the CMake `example` files.
+
+    :param filename: the XML file to parse
+    :param name: name of subdirectory to be created under examples/c++/
+    :return: nothing
+    """
     parser = ParseXML.ParseXML(filename)
     ob = parser.parse_deviser_xml()
     os.chdir(os.path.normpath('./temp'))
@@ -67,11 +85,28 @@ def generate_cmake_example_files(filename, name):
 # Specific compare functions
 
 def compare_files(correct_file, temp_file):
+    """
+    Do a string comparison of the contents of two files.
+
+    :param correct_file: reference file
+    :param temp_file: file generated during tests
+    :return: 0 on success, or file not present; 1 on failure.
+    """
     return test_functions.compare_files(correct_file, temp_file, fails,
                                         not_tested)
 
 
 def compare_cmake(name, is_src=False):
+    """
+    Do a string comparison of the contents of two files.
+
+    TODO: the two cases checked by this function are identical!
+    Presumably a mistake?
+
+    :param name: TODO
+    :param is_src: TODO
+    :return: 0 on success, or file not present; 1 on failure.
+    """
     if is_src:
         correct_file = os.path.normpath('./test-cmake/{0}-package.cmake'.format(name))
         temp_file = os.path.normpath('./temp/{0}-package.cmake'.format(name))
@@ -82,6 +117,13 @@ def compare_cmake(name, is_src=False):
 
 
 def compare_cmake_register(name, is_cxx=False):
+    """
+    Compare two cmake 'register' files.
+
+    :param name: first part of filename
+    :param is_cxx: Use True if a .cxx file, otherwise False
+    :return: 0 on success, or file not present; 1 on failure.
+    """
     if is_cxx:
         correct_file = os.path.normpath('./test-cmake/{0}-register.h'.format(name))
         temp_file = os.path.normpath('./temp/{0}-register.h'.format(name))
@@ -90,7 +132,16 @@ def compare_cmake_register(name, is_cxx=False):
         temp_file = os.path.normpath('./temp/{0}-register.cxx'.format(name))
     return compare_files(correct_file, temp_file)
 
+
 def compare_cmake_example(name, is_txt=False):
+    """
+    Compare two cmake 'register' files.
+
+    :param name: first part of filename, or directory name if
+                 'is_txt' is True and name is 'spatial'
+    :param is_txt: Use True if files end in .txt, otherwise False
+    :return: 0 on success, or file not present; 1 on failure.
+    """
     if is_txt and name=='spatial':
         correct_file = os.path.normpath('./test-cmake/examples/CMakeLists.txt')
         temp_file = os.path.normpath('./temp/examples/c++/{0}/CMakeLists.txt'.format(name))
@@ -99,7 +150,14 @@ def compare_cmake_example(name, is_txt=False):
         temp_file = os.path.normpath('./temp/examples/{0}-package.cmake'.format(name))
     return compare_files(correct_file, temp_file)
 
+
 def compare_examples(name):
+    """
+    Compare two 'example' files.
+
+    :param name: first part of filename, and a subdir under temp/examples/c++/
+    :return: 0 on success, or file not present; 1 on failure.
+    """
     correct_file = os.path.normpath('./test-cmake/examples/c++/{0}_example1.cpp'.format(name))
     temp_file = os.path.normpath('./temp/examples/c++/{0}/{0}_example1.cpp'.format(name))
     return compare_files(correct_file, temp_file)
@@ -109,6 +167,12 @@ def compare_examples(name):
 
 
 def run_cmake_test(name):
+    """
+    General CMake test.
+
+    :param name: stub of XML filename, e.g. 'dyn' for filename 'dyn.xml'
+    :return: 0 if all comparisons succeed, else number of failures.
+    """
     filename = test_functions.set_up_test(name, 'CMake')
     generate_cmake_files(filename)
     fail = compare_cmake(name)
@@ -118,6 +182,12 @@ def run_cmake_test(name):
 
 
 def run_register_test(name):
+    """
+    CMake 'register' test.
+
+    :param name: stub of XML filename, e.g. 'dyn' for filename 'dyn.xml'
+    :return: 0 if all comparisons succeed, else number of failures.
+    """
     filename = test_functions.set_up_test(name, 'CMake', 'register')
     generate_cmake_register_files(filename)
     fail = compare_cmake_register(name)
@@ -127,6 +197,12 @@ def run_register_test(name):
 
 
 def run_example_test(name):
+    """
+    CMake 'example' test.
+
+    :param name: stub of XML filename, e.g. 'dyn' for filename 'dyn.xml'
+    :return: 0 if all comparisons succeed, else number of failures.
+    """
     filename = test_functions.set_up_test(name, 'CMake', 'example')
     generate_cmake_example_files(filename, name)
     fail = compare_cmake_example(name)
@@ -142,6 +218,9 @@ def run_example_test(name):
 
 
 def main():
+    """
+    Main function. Set up and run all tests.
+    """
 
     # Set up the environment.
     this_dir = os.path.dirname(os.path.abspath(__file__))
