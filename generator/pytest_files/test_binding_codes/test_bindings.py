7e85efe169c444ef1d9e4dd91f4e18036236f905
Matthew Gillman <m.gillman@ucl.ac.uk>
2021-03-30 11:43:51 +0100

    Added the rest of the bindings tests to pytest.

--- generator/pytest_files/test_binding_codes/test_bindings.py
+++ generator/pytest_files/test_binding_codes/test_bindings.py
@@ -31,6 +31,55 @@ def test_something():
     ('spatial', 'python', 'cpp'),
     ('spatial', 'r', 'cpp'),
     ('spatial', 'ruby', 'cpp'),
+    ('test_vers', 'java', 'i'),
 ])
 def test_basic(name, binding, ext):
     assert rbt.run_test(name, binding, ext) == 0
+
+
+@pytest.mark.parametrize("name, binding, ext", [
+    ('spatial', 'csharp', 'i'),
+    ('spatial', 'java', 'i'),
+    ('spatial', 'javascript', 'cpp'),
+    ('spatial', 'perl', 'cpp'),
+    ('spatial', 'php', 'cpp'),
+    ('spatial', 'python', 'cpp'),
+    ('spatial', 'r', 'cpp'),
+    ('spatial', 'ruby', 'cpp'),
+    ('test_vers', 'java', 'i'),
+])
+def test_ns(name, binding, ext):
+    assert rbt.run_ns_test(name, binding, ext) == 0
+
+
+@pytest.mark.parametrize("name, binding, ext", [
+    ('spatial', 'javascript', 'cpp'),
+    ('spatial', 'java', 'i'),
+    ('test_vers', 'java', 'i'),
+])
+def test_pkgs(name, binding, ext):
+    assert rbt.run_pkgs_test(name, binding, ext) == 0
+
+
+@pytest.mark.parametrize("name, binding, ext, local", [
+    ('spatial', 'csharp', 'i', 'False'),
+    ('spatial', 'php', 'i', 'True'),
+])
+def test_local(name, binding, ext, local):
+    assert rbt.run_local_test(name, binding, ext, local) == 0
+
+
+@pytest.mark.parametrize("name, binding, ext", [
+    ('spatial', 'ruby', 'cpp'),
+    ('qual', 'javascript', 'cpp'),
+])
+def test_plugin(name, binding, ext):
+    assert rbt.run_plugin_test(name, binding, ext) == 0
+
+
+@pytest.mark.parametrize("name, binding, ext", [
+    ('spatial', 'swig', 'i'),
+    ('spatial', 'swig', 'h'),
+])
+def test_swig(name, binding, ext):
+    assert rbt.run_swig_test(name, binding, ext) == 0
