d80527ad609f192b38dda1e795fdbf53edf38167
Matthew Gillman <m.gillman@ucl.ac.uk>
2021-03-31 17:05:44 +0100

    Added test_many_things() func for strfunc under pytest.

--- generator/pytest_files/test_strfunc/test_strfunc.py
+++ generator/pytest_files/test_strfunc/test_strfunc.py
@@ -5,9 +5,6 @@ from ...tests import test_functions
 from ...tests.test_strfunc_code import run_strfunc_tests as rst
 from ...util import strFunctions as sf, global_variables as gv
 
-def test_something():
-    assert (1 == 1)
-
 
 @pytest.mark.parametrize('func, input, expected_output, kwargs', [
     (sf.upper_first, 'cat', 'Cat', {}),
@@ -29,3 +26,26 @@ def test_one_thing(func, input, expected_output, kwargs):
     #    actual_output = func(input)
     actual_output = func(input, **kwargs)
     assert actual_output == expected_output
+
+
+catdat = {'cat': 'Cat', 'csgsomething': 'CSGsomething', 'csgcat': 'CSGcat',
+            'cscat': 'Cscat', 'csgeometry': 'CSGeometry',
+            'csGeometry': 'CSGeometry', 'a': 'A'}
+
+@pytest.mark.parametrize('func, test_data, kwargs', [
+    (sf.upper_first, catdat, {}),
+    (sf.lower_first, rst.swap_dictionary(catdat), {}),
+])
+def test_many_things(func, test_data, kwargs):
+    """
+    Execute a set of individual tests on a strFunc function.
+
+    :param func: the function under test
+    :param test_data: dictionary of test data
+                      (keys=inputs, values=expected outputs)
+    :param kwargs: dictionary of any named arguments for the function
+    """
+    for (input, expected) in test_data.items():
+        actual_output = func(input, **kwargs)
+        assert actual_output == expected
+
