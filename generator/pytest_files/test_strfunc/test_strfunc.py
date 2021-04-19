01b4e7fb76d57a8defb4417b68d39a6cb86d5c7b
Matthew Gillman <m.gillman@ucl.ac.uk>
2021-04-19 17:13:01 +0100

    Renamed function argument to stop pylint complaining I'm overriding a built-in.

--- generator/pytest_files/test_strfunc/test_strfunc.py
+++ generator/pytest_files/test_strfunc/test_strfunc.py
@@ -25,7 +25,7 @@ attribute10 = {'type': 'lo_element', 'element': 'Unit', 'name': 'Unit',
 
 # Please try and keep functions in same order as in the strFunctions.py file
 # for ease of reference (and to see what is missing).
-@pytest.mark.parametrize('func, input, expected_output, kwargs, do_first', [
+@pytest.mark.parametrize('func, test_input, expected_output, kwargs, do_first', [
     (sf.upper_first, 'cat', 'Cat', {}, ''),
 
     (sf.wrap_token, 'fred', '\\token{cat:\\-fred}', {'pkg': 'cat'}, ''),
@@ -85,13 +85,13 @@ attribute10 = {'type': 'lo_element', 'element': 'Unit', 'name': 'Unit',
     (sf.get_class_from_plugin, "Plugin", "", {"package": "Comp"}, ''),
 
 ])
-def test_one_thing(func, input, expected_output, kwargs, do_first):
+def test_one_thing(func, test_input, expected_output, kwargs, do_first):
     """
     Run a single test case on a strFunc function, and check that
     the actual output returned is the same as that expected.
 
     :param func: the strFunc function under test
-    :param input: input to function
+    :param test_input: input to function
     :param expected_output: what we expect the function to return.
            Can be None.
     :param kwargs: dictionary of keyword arguments, if any, for the function
@@ -106,7 +106,7 @@ def test_one_thing(func, input, expected_output, kwargs, do_first):
     #    actual_output = func(input)
     if do_first:
         exec(do_first)
-    actual_output = func(input, **kwargs)
+    actual_output = func(test_input, **kwargs)
     assert actual_output == expected_output
 
 
