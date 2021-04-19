170bb5095611de0c22cf5cc5c73d8d824985aa68
Matthew Gillman <m.gillman@ucl.ac.uk>
2021-04-19 17:19:51 +0100

    Initial commit for test_other_library under pytest.

---
+++ generator/pytest_files/test_other_library/test_other_library.py
@@ -0,0 +1,8 @@
+# Matthew S. Gillman, UCL, 19th April 2021.
+import pytest
+
+from ...tests.test_other_library import run_other_library_tests as rolt
+from ...util import strFunctions as sf, global_variables as gv
+
+def test_something():
+    assert 1 == 1
