0f64f2483dc2210cdf1d50ed7e166962e416b4e4
Matthew Gillman <m.gillman@ucl.ac.uk>
2021-04-19 16:50:35 +0100

    Fixed flake8 complaints.

--- generator/pytest_files/test_strfunc/test_strfunc.py
+++ generator/pytest_files/test_strfunc/test_strfunc.py
@@ -1,8 +1,6 @@
 # Matthew S. Gillman, UCL, 31st March 2021.
-import os
 import pytest
 
-from ...tests import test_functions
 from ...tests.test_strfunc_code import run_strfunc_tests as rst
 from ...util import strFunctions as sf, global_variables as gv
 
@@ -23,6 +21,7 @@ attribute9 = {'type': 'lo_element', 'element': 'Unit',
               'name': 'FooUnit', 'isListOf': True}
 attribute10 = {'type': 'lo_element', 'element': 'Unit', 'name': 'Unit',
                'isListOf': True, 'listOfClassName': 'ListOfUnits'}
+
 
 # Please try and keep functions in same order as in the strFunctions.py file
 # for ease of reference (and to see what is missing).
@@ -50,28 +49,38 @@ attribute10 = {'type': 'lo_element', 'element': 'Unit', 'name': 'Unit',
     (sf.get_element_name, attribute4, "Unit", {}, ''),
     (sf.get_element_name, attribute5, "\\BarListOfUnits", {}, ''),
     # (sf.cap_list_of_name, 'Unit', 'ListOfUnits', {'add_prefix': False}, ''),
-    (sf.get_element_name, attribute5, "\\ListOfUnits", {'add_prefix_if_not_pkg': False}, ''),
-    (sf.get_element_name, attribute5, "\\ListOfUnits", {'leave_pkg_prefix': False}, ''),
+    (sf.get_element_name, attribute5, "\\ListOfUnits",
+     {'add_prefix_if_not_pkg': False}, ''),
+    (sf.get_element_name, attribute5, "\\ListOfUnits",
+     {'leave_pkg_prefix': False}, ''),
     (sf.get_element_name, attribute6, "FIX_ME", {}, ''),
     (sf.get_element_name, attribute7, "\\Foo", {}, ''),
     (sf.get_element_name, attribute8, "\\BarListOfUnits", {}, ''),
-    (sf.get_element_name, attribute8, "\\ListOfAnything", {'leave_pkg_prefix': False}, ''),
+    (sf.get_element_name, attribute8, "\\ListOfAnything",
+     {'leave_pkg_prefix': False}, ''),
     (sf.get_element_name, attribute9, "\\ListOfFooUnits", {},
-        'gv.reset(); gv.prefix = "Bar"; gv.is_package = True; gv.package_prefix = "Foo"'),
+     'gv.reset(); gv.prefix = "Bar"; gv.is_package = True;' \
+     ' gv.package_prefix = "Foo"'),
     (sf.get_element_name, attribute9, "\\ListOfFooUnits",
         {'add_prefix_if_not_pkg': 'False'}, ''),
     (sf.get_element_name, attribute10, "\\ListOfUnits", {}, ''),
-    (sf.get_element_name, attribute10, "\\ListOfUnits", {'leave_pkg_prefix': False}, ''),
-    (sf.get_element_name, attribute10, "\\ListOfUnits", {'add_prefix_if_not_pkg': False}, ''),
+    (sf.get_element_name, attribute10, "\\ListOfUnits",
+     {'leave_pkg_prefix': False}, ''),
+    (sf.get_element_name, attribute10, "\\ListOfUnits",
+     {'add_prefix_if_not_pkg': False}, ''),
 
     (sf.replace_digits, "John 3:16", "John Three:OneSix", {}, ''),
 
-    (sf.texify, "012_27 cat44_8 ", "ZeroOneTwo\\_TwoSevencatFourFour\\_Eight", {}, ''),
+    (sf.texify, "012_27 cat44_8 ", "ZeroOneTwo\\_TwoSevencatFourFour\\_Eight",
+     {}, ''),
 
-    (sf.compare_no_case, "This iS a TEST", True, {"reference": "this is a test"}, ''),
-    (sf.compare_no_case, "This iS a bad TEST", False, {"reference": "this is a test"}, ''),
+    (sf.compare_no_case, "This iS a TEST", True,
+     {"reference": "this is a test"}, ''),
+    (sf.compare_no_case, "This iS a bad TEST", False,
+     {"reference": "this is a test"}, ''),
 
-    (sf.get_class_from_plugin, "CompModelPlugin", "Model", {"package": "Comp"}, ''),
+    (sf.get_class_from_plugin, "CompModelPlugin", "Model",
+     {"package": "Comp"}, ''),
     (sf.get_class_from_plugin, "CompModelPlug", "", {"package": "Comp"}, ''),
     (sf.get_class_from_plugin, "Plugin", "", {"package": "Comp"}, ''),
 
@@ -83,16 +92,17 @@ def test_one_thing(func, input, expected_output, kwargs, do_first):
 
     :param func: the strFunc function under test
     :param input: input to function
-    :param expected_output: what we expect the function to return. Can be None.
+    :param expected_output: what we expect the function to return.
+           Can be None.
     :param kwargs: dictionary of keyword arguments, if any, for the function
            under test.
     :param do_first: string of command(s) (separated by semi-colons)
            to be executed before running the test.
            Example: do_first = 'ans = 2 + 2; print(ans)'
     """
-    #if kwargs:
+    # if kwargs:
     #    actual_output = func(input, **kwargs)
-    #else:
+    # else:
     #    actual_output = func(input)
     if do_first:
         exec(do_first)
@@ -106,15 +116,16 @@ def test_one_thing(func, input, expected_output, kwargs, do_first):
 # NB Those dictionaries which are only used once I have (mainly?) placed
 # in the fixture itself.
 catdat = {'cat': 'Cat', 'csgsomething': 'CSGsomething', 'csgcat': 'CSGcat',
-            'cscat': 'Cscat', 'csgeometry': 'CSGeometry',
-            'csGeometry': 'CSGeometry', 'a': 'A'}
+          'cscat': 'Cscat', 'csgeometry': 'CSGeometry',
+          'csGeometry': 'CSGeometry', 'a': 'A'}
 foodat = {"FooParameter": "ListOfFooParameters"}
 foxcatchild = {"fox": "listOfFoxes", "cat": "listOfCats",
-             "child": "listOfChildren"}
+               "child": "listOfChildren"}
 foocat = {"FooParameter": "ListOfFooParameters", "cat": "ListOfCats"}
-foosbmlcat = {"FooParameter": "SBMLListOfFooParameters", "cat": "SBMLListOfCats"}
+foosbmlcat = {"FooParameter": "SBMLListOfFooParameters",
+              "cat": "SBMLListOfCats"}
 foolistcat = {"FooParameter": "ListOfFooParameters", "cat": "ListOfCats",
-            "SBMLFooParameter": "ListOfSBMLFooParameters"}
+              "SBMLFooParameter": "ListOfSBMLFooParameters"}
 foolistcat2 = {"FooParameter": "ListOfFooParameters", "cat": "ListOfCats",
                "SBMLFooParameter": "ListOfFooParameters"}
 plurals = {"fox": "foxes", "child": "children", "SBMLchild": "SBMLchildren"}
@@ -138,78 +149,103 @@ prefix_data = {"FbcType": "FbcType", "FluxObjective": "FluxObjective",
     (sf.upper_first, catdat, {}, ''),
     (sf.lower_first, rst.swap_dictionary(catdat), {}, ''),
 
-    (sf.abbrev_name, {"thisIsATest": "iat", "CAT": "cat", "cat": "c", "c": "c", "C": "c"}, {}, ''),
-    (sf.abbrev_lo_name, {"spaghetti": "", "ListOfFoo": "LOFoo", "ListOfFOO": "LOFOO"}, {}, ''),
+    (sf.abbrev_name,
+     {"thisIsATest": "iat", "CAT": "cat", "cat": "c", "c": "c", "C": "c"},
+     {}, ''),
+    (sf.abbrev_lo_name,
+     {"spaghetti": "", "ListOfFoo": "LOFoo", "ListOfFOO": "LOFOO"}, {}, ''),
 
     (sf.list_of_name, foodat, {'add_prefix': True}, 'gv.reset()'),
     (sf.list_of_name, foodat, {'add_prefix': False}, ''),
     (sf.list_of_name, foodat, {'add_prefix': False}, 'gv.is_package = False'),
-    (sf.list_of_name, {"FooParameter": "ListOfFooParameters"}, {'add_prefix': False}, ''),
-    (sf.list_of_name, {"FooParameter": "ListOfFooParameters"}, {'add_prefix': True}, 'gv.is_package = True'),
-    (sf.list_of_name, {"FooParameter": "SBMLListOfFooParameters"}, {'add_prefix': True}, 'gv.is_package = False'),
+    (sf.list_of_name, {"FooParameter": "ListOfFooParameters"},
+     {'add_prefix': False}, ''),
+    (sf.list_of_name, {"FooParameter": "ListOfFooParameters"},
+     {'add_prefix': True}, 'gv.is_package = True'),
+    (sf.list_of_name, {"FooParameter": "SBMLListOfFooParameters"},
+     {'add_prefix': True}, 'gv.is_package = False'),
 
     (sf.lower_list_of_name_no_prefix, foxcatchild, {}, 'gv.reset()'),
-    (sf.lower_list_of_name_no_prefix, foxcatchild, {}, 'gv.is_package = False'),
+    (sf.lower_list_of_name_no_prefix, foxcatchild, {},
+     'gv.is_package = False'),
 
     (sf.cap_list_of_name, foocat, {'add_prefix': False}, 'gv.reset()'),
     (sf.cap_list_of_name, foocat, {'add_prefix': True}, ''),
-    (sf.cap_list_of_name, foocat, {'add_prefix': False}, 'gv.is_package = False'),
+    (sf.cap_list_of_name, foocat, {'add_prefix': False},
+     'gv.is_package = False'),
     (sf.cap_list_of_name, foosbmlcat, {'add_prefix': True}, ''),
     (sf.cap_list_of_name, {'Unit': 'ListOfUnits'}, {'add_prefix': False}, ''),
-    (sf.cap_list_of_name, {'Unit': 'SBMLListOfUnits'}, {'add_prefix': True}, ''),
+    (sf.cap_list_of_name, {'Unit': 'SBMLListOfUnits'}, {'add_prefix': True},
+     ''),
 
     (sf.cap_list_of_name_no_prefix, foolistcat, {}, 'gv.reset()'),
-    # The next one this wrongly dealt with by the remove prefix function: TODO FIXIT
-    # (sf.cap_list_of_name_no_prefix, foolistcat2, {}, 'gv.is_package = False'),
+    # The next one this wrongly dealt with by the remove prefix function:
+    # TODO FIXIT
+    # (sf.cap_list_of_name_no_prefix, foolistcat2, {},
+    #   'gv.is_package = False'),
     (sf.cap_list_of_name_no_prefix, {'fox': 'ListOfFoxes'}, {}, ''),
     # (sf.cap_list_of_name_no_prefix, {'SBMLFoo': 'ListOfFoos'}, {}, ''),
 
     (sf.plural_no_prefix, plurals, {}, 'gv.reset()'),
-    (sf.plural_no_prefix, rosieplurals, {}, 'gv.is_package = False; gv.prefix = "Rosie"'),
+    (sf.plural_no_prefix, rosieplurals, {},
+     'gv.is_package = False; gv.prefix = "Rosie"'),
     (sf.plural_no_prefix, rosieplurals2, {}, 'gv.is_package = True'),
 
     (sf.plural, lots_of_plurals, {}, ''),
 
-    (sf.singular, rst.swap_dictionary(lots_of_plurals), {}, 'test_data["dogs"] = "dog"'),
+    (sf.singular, rst.swap_dictionary(lots_of_plurals), {},
+     'test_data["dogs"] = "dog"'),
 
     # plural_then_single and vice versa here.
     # Keys = Values (inputs = expected outputs)
     # Dictionary comprehension: data = {k: k for k in ['cat', 'dog', 'bike']}
     (rst.plural_then_single,
-       {k: k for k in ["cat", "dog", "child", "disinformation", "fox",
-                       "party", "cloud", "something", "apple"]}, {}, ''),
+     {k: k for k in ["cat", "dog", "child", "disinformation", "fox",
+                     "party", "cloud", "something", "apple"]}, {}, ''),
     (rst.single_then_plural,
-       {k: k for k in ["cats", "dogs", "children", "disinformation", "foxes",
-                       "parties", "clouds", "somethings", "apples"]}, {}, ''),
+     {k: k for k in ["cats", "dogs", "children", "disinformation", "foxes",
+                     "parties", "clouds", "somethings", "apples"]}, {}, ''),
 
 
     # Lots of remove_prefix() tests - this is a complicated function to test!
     # tests removing a package prefix. gv.reset() sets gv.prefix to "SBML"
     (sf.remove_prefix, fbc, {},
-            'gv.reset(); gv.is_package = True; gv.package_prefix = "Fbc"'),
+     'gv.reset(); gv.is_package = True; gv.package_prefix = "Fbc"'),
     (sf.remove_prefix, fbc, {'remove_package': True}, ''),
     (sf.remove_prefix, {"FbcSBMLDocument": "SBMLDocument"},
-         {'remove_doc_prefix': True}, ''),
+     {'remove_doc_prefix': True}, ''),
     (sf.remove_prefix, fbc, {'prefix': 'Fbc'},
         'gv.reset(); gv.is_package = True; gv.package_prefix = ""'),
     # No prefix to remove has been specified or explicitly told not to remove:
     (sf.remove_prefix, prefix_data, {'prefix': ''}, ''),
-    (sf.remove_prefix, prefix_data, {'prefix': 'Fbc', 'remove_package': False}, ''),
+    (sf.remove_prefix, prefix_data,
+     {'prefix': 'Fbc', 'remove_package': False}, ''),
     (sf.remove_prefix, prefix_data, {'remove_doc_prefix': True}, ''),
-    (sf.remove_prefix, prefix_data, {'prefix': '', 'remove_doc_prefix': True, }, ''),
-    (sf.remove_prefix, prefix_data, {'prefix': 'Fbc', 'remove_doc_prefix': False, 'remove_package': False}, ''),
-    (sf.remove_prefix, {"FbcSBMLDocument": "SBMLDocument"}, {'prefix': 'Fbc', 'remove_doc_prefix': True}, ''),
-    (sf.remove_prefix, {"RosieFbcType": "FbcType", "RosieDocument": "RosieDocument"},
-             {'prefix': 'this can be anything!!!'}, 'gv.reset(); gv.prefix = "Rosie"; gv.is_package = False'),
-    (sf.remove_prefix, {"RosieFbcType": "FbcType", "RosieDocument": "Document"},
-             {'prefix': 'this can be anything!!!', 'remove_doc_prefix': True}, ''),
-    (sf.remove_prefix, {"PAZZFbcType": "FbcType", }, {}, 'gv.prefix = "PAZZ" '),
-    (sf.remove_prefix, {"PAZZDocument": "PAZZDocument", }, {}, 'gv.prefix = "PAZZ" '),
+    (sf.remove_prefix, prefix_data,
+     {'prefix': '', 'remove_doc_prefix': True, }, ''),
+    (sf.remove_prefix, prefix_data, {'prefix': 'Fbc',
+                                     'remove_doc_prefix': False,
+                                     'remove_package': False}, ''),
+    (sf.remove_prefix, {"FbcSBMLDocument": "SBMLDocument"},
+     {'prefix': 'Fbc', 'remove_doc_prefix': True}, ''),
+    (sf.remove_prefix,
+     {"RosieFbcType": "FbcType", "RosieDocument": "RosieDocument"},
+     {'prefix': 'this can be anything!!!'},
+     'gv.reset(); gv.prefix = "Rosie"; gv.is_package = False'),
+    (sf.remove_prefix, {"RosieFbcType": "FbcType",
+                        "RosieDocument": "Document"},
+     {'prefix': 'this can be anything!!!', 'remove_doc_prefix': True}, ''),
+    (sf.remove_prefix, {"PAZZFbcType": "FbcType", }, {},
+     'gv.prefix = "PAZZ" '),
+    (sf.remove_prefix, {"PAZZDocument": "PAZZDocument", }, {},
+     'gv.prefix = "PAZZ" '),
     (sf.remove_prefix, {"PAZZMatt": "Matt"}, {}, 'gv.prefix = "PAZZ" '),
 
-    (sf.is_prefixed_name, {"RosieFox": True, "rosieFoo": True, "rosiefoo": False,
-            "RosiFox": False, "RoSiEFoo": True, "RoSiEfoo": False},
-            {'prefix': 'Rosie'}, 'gv.reset()'),  # gv.reset() means gv.prefix is now "SBML"
+    (sf.is_prefixed_name, {"RosieFox": True, "rosieFoo": True,
+                           "rosiefoo": False, "RosiFox": False,
+                           "RoSiEFoo": True, "RoSiEfoo": False},
+     {'prefix': 'Rosie'},
+     'gv.reset()'),  # gv.reset() means gv.prefix is now "SBML"
 
     (sf.is_prefixed_document_class,
         {"RosieDocument": True, "rosieDocument": True,
@@ -306,10 +342,10 @@ def test_prefix_classes():
     attribs_before = [attrib1_before, attrib2_before]
     attribs_after = [attrib1_after, attrib2_after]
     changing_dict_2 = {'name': 'Colin', 'baseClass': gv.baseClass,
-                     'lo_class_name': 'Dabs', 'attribs': attribs_before}
+                       'lo_class_name': 'Dabs', 'attribs': attribs_before}
     expected_dict_2 = {'name': 'SBMLColin', 'baseClass': gv.baseClass,
-                     'elementName': 'colin', 'lo_class_name': 'SBMLDabs',
-                     'attribs': attribs_after}
+                       'elementName': 'colin', 'lo_class_name': 'SBMLDabs',
+                       'attribs': attribs_after}
     sf.prefix_classes(changing_dict_2)
     assert changing_dict_2 == expected_dict_2
 
@@ -341,5 +377,6 @@ def test_list_of_name():
     gv.is_package = True
     assert sf.list_of_name('FooParameter', True) == "ListOfFooParameters"
     assert sf.list_of_name('FooParameter', False) == "ListOfFooParameters"
-    # (sf.list_of_name, {"FooParameter": "ListOfFooParameters"}, {'add_prefix': True},
+    # (sf.list_of_name, {"FooParameter": "ListOfFooParameters"},
+    #   {'add_prefix': True},
     # 'gv.prefix = "PAZZ"; gv.is_package = True'),
