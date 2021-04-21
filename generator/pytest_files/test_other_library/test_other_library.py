b956990b60fe4ccb8ad4b8e1bd51741bc24bd5c7
Matthew Gillman <m.gillman@ucl.ac.uk>
2021-04-21 17:35:05 +0100

    Fixed flake8 complaints.

--- generator/pytest_files/test_other_library/test_other_library.py
+++ generator/pytest_files/test_other_library/test_other_library.py
@@ -1,5 +1,6 @@
 # Matthew S. Gillman, UCL, 19th April 2021.
-import os, pytest
+import os
+import pytest
 
 from ...tests import test_functions
 from ...tests.test_other_library import run_other_library_tests as rolt
@@ -21,13 +22,17 @@ def setup():
     ('test_sedml', 0, 'SedDocument', 'document', ''),
     ('test_sedml', 3, 'SedAddXML', 'xmlnode used', ''),
     ('test_sedml', 26, 'SedSetValue', 'astnode used', ''),
-    ('test_sedml', 12, 'SedDataGenerator', 'astnode in getElementBySId bug', ''),
-    ('test_sedml', 33, 'SedRepeatedTask', 'attribute with different xml name', ''),
+    ('test_sedml', 12, 'SedDataGenerator', 'astnode in getElementBySId bug',
+     ''),
+    ('test_sedml', 33, 'SedRepeatedTask', 'attribute with different xml name',
+     ''),
     ('test_sedml', 7, 'SedSimulation', 'child element', ''),
     ('test_sedml', 28, 'SedVectorRange', 'deal with vectors', ''),
-    ('test_sedml', 10, 'SedAbstractTask', 'catch different abstract types', 'SedListOfTasks'),
+    ('test_sedml', 10, 'SedAbstractTask', 'catch different abstract types',
+     'SedListOfTasks'),
     # TODO: shouldn't next one's description be about different output types??
-    ('test_sedml', 13, 'SedOutput', 'catch different abstract types', 'SedListOfOutputs'),
+    ('test_sedml', 13, 'SedOutput', 'catch different abstract types',
+     'SedListOfOutputs'),
 
     # Tests from old rolt.testCombine()
     ('combine-archive', 0, 'CaContent', 'check includes', 'CaListOfContents'),
@@ -39,15 +44,18 @@ def test_cpp(name, num, class_name, test_case, list_of):
     Based on rolt.run_test(). Run the C++ file tests.
 
     :param name: name of test group e.g. 'test_sedml', 'combine-archive'.
-    :param num: list index, so we only generate a particular class not all of them
-    :param class_name: name of C++ class (and thus .cpp/.h filenames), e.g. 'CaContent'
+    :param num: list index, so we only generate a
+                particular class, not all of them
+    :param class_name: name of C++ class (and thus .cpp/.h filenames),
+                e.g. 'CaContent'
     :param test_case: brief test description
-    :param list_of: class name (and thus filenames) of any corresponding "list of" class, e.g. 'CaListOfContents'
+    :param list_of: class name (and thus filenames) of any corresponding
+                "list of" class, e.g. 'CaListOfContents'
     """
-    gv.reset()  # NB do we need to do this for every test? Will it break anything?
-    filename = test_functions.set_up_test(name, class_name, test_case)  # XML library file.
-    assert filename is not None and filename != ""
-    rolt.generate_new_cpp_header(filename, num)
+    gv.reset()
+    xml_filename = test_functions.set_up_test(name, class_name, test_case)
+    assert xml_filename is not None and xml_filename != ""
+    rolt.generate_new_cpp_header(xml_filename, num)
     assert 0 == rolt.compare_code_headers(class_name)
     assert 0 == rolt.compare_code_impl(class_name)
     if len(list_of) > 0:
@@ -66,14 +74,16 @@ def test_templates(name, class_name, test_case, list_of):
     Based on old rolt.run_templates() function. Run the 'template' file tests.
 
     :param name: name of test group e.g. 'test_sedml', 'combine-archive'.
-    :param class_name: name of C++ class (and thus .cpp/.h filenames), e.g. 'SedBase'
+    :param class_name: name of C++ class (and thus .cpp/.h filenames),
+                 e.g. 'SedBase'
     :param test_case: brief test description, e.g. 'templates'.
-    :param list_of: class name (and thus filenames) of any corresponding "list of" class, e.g. 'SedListOf'
+    :param list_of: class name (and thus filenames) of any corresponding
+                 "list of" class, e.g. 'SedListOf'
     """
     gv.reset()
-    filename = test_functions.set_up_test(name, class_name, test_case)
-    assert filename is not None and filename != ""
-    rolt.generate_templates(filename)
+    xml_filename = test_functions.set_up_test(name, class_name, test_case)
+    assert xml_filename is not None and xml_filename != ""
+    rolt.generate_templates(xml_filename)
     assert 0 == rolt.compare_code_headers(class_name)
     assert 0 == rolt.compare_code_impl(class_name)
     assert 0 == rolt.compare_code_headers(list_of)
@@ -97,14 +107,16 @@ def test_common_templates(name, class_name, test_case, prefix, lib):
     :return: number of failed tests.
     """
     gv.reset()
-    filename = test_functions.set_up_test(name, class_name, test_case)
-    rolt.generate_common_templates(filename)
+    xml_filename = test_functions.set_up_test(name, class_name, test_case)
+    rolt.generate_common_templates(xml_filename)
     assert 0 == rolt.compare_code_headers('common')
     assert 0 == rolt.compare_code_headers('extern')
     assert 0 == rolt.compare_code_headers('lib{0}-config'.format(lib))
     assert 0 == rolt.compare_code_impl('lib{0}-version'.format(lib))
-    assert 0 == rolt.compare_code_headers('{0}OperationReturnValues'.format(prefix))
-    assert 0 == rolt.compare_code_impl('{0}OperationReturnValues'.format(prefix))
+    assert 0 == rolt.compare_code_headers('{0}OperationReturnValues'.
+                                          format(prefix))
+    assert 0 == rolt.compare_code_impl('{0}OperationReturnValues'.
+                                       format(prefix))
     assert 0 == rolt.compare_code_cmake('lib{0}-version.h'.format(lib))
     assert 0 == rolt.compare_code_cmake('lib{0}-config-common.h'.format(lib))
     assert 0 == rolt.compare_code_cmake('lib{0}-namespace.h'.format(lib))
@@ -118,13 +130,14 @@ def test_enum(name, class_name, test_case):
     Based on old run_enum() function.
     Run extension types file tests.
 
-    :param name: stub of XML file name, e.g. 'test_sedml' for file test_sedml.xml.
+    :param name: stub of XML file name, e.g. 'test_sedml' for
+               file test_sedml.xml.
     :param class_name: test class, e.g. 'SedmlEnumerations'
     :param test_case: brief description of tests, e.g. 'enumerations'.
     """
     gv.reset()
-    filename = test_functions.set_up_test(name, class_name, test_case)
-    rolt.generate_enum(filename)
+    xml_filename = test_functions.set_up_test(name, class_name, test_case)
+    rolt.generate_enum(xml_filename)
     assert 0 == rolt.compare_code_headers(class_name)
     assert 0 == rolt.compare_code_impl(class_name)
 
@@ -140,30 +153,37 @@ def test_bindings(name, class_name, test_case, binding, prefix):
     Based on old test_bindings() function.
     Compare the 'bindings' files.
 
-    :param name: stub of XML filename, e.g. 'test_sedml' for file test_sedml.xml.
+    :param name: stub of XML filename, e.g. 'test_sedml' for
+              file test_sedml.xml.
     :param class_name: the test class, e.g. 'libsedml'
     :param test_case: brief description of tests, e.g. 'swig dir'
     :param binding: the binding type, e.g. 'swig'
-    :param prefix: used to match library files, e.g. 'combine' matches libcombine-*.*
+    :param prefix: used to match library files, e.g. 'combine' matches
+              libcombine-*.*
     """
     gv.reset()
-    filename = test_functions.set_up_test(name, class_name, test_case)
-    rolt.generate_binding(filename, binding)
+    xml_filename = test_functions.set_up_test(name, class_name, test_case)
+    rolt.generate_binding(xml_filename, binding)
     if binding == 'swig':
         assert 0 == rolt.compare_binding_headers('ListWrapper', binding, "")
         assert 0 == rolt.compare_binding_headers('OStream', binding, "")
         assert 0 == rolt.compare_binding_impl('OStream', binding, "")
         assert 0 == rolt.compare_binding_interface('std_set', binding, "")
-        assert 0 == rolt.compare_binding_headers('lib{0}'.format(prefix), binding, "")
-        assert 0 == rolt.compare_binding_interface('lib{0}'.format(prefix), binding, "")
+        assert 0 == rolt.compare_binding_headers('lib{0}'.format(prefix),
+                                                 binding, "")
+        assert 0 == rolt.compare_binding_interface('lib{0}'.format(prefix),
+                                                   binding, "")
         assert 0 == rolt.compare_binding_interface('ASTNodes', binding, "")
     elif binding == 'csharp':
         assert 0 == rolt.compare_binding_impl('local', binding, "")
         assert 0 == rolt.compare_binding_interface('local', binding, "")
-        assert 0 == rolt.compare_binding_interface('lib{0}'.format(prefix), binding, "")
+        assert 0 == rolt.compare_binding_interface('lib{0}'.format(prefix),
+                                                   binding, "")
         assert 0 == rolt.compare_binding_file('CMakeLists.txt', binding, "")
-        assert 0 == rolt.compare_binding_file('compile-native-files.cmake', binding, "")
-        assert 0 == rolt.compare_binding_file('AssemblyInfo.cs.in', binding, "")
+        assert 0 == rolt.compare_binding_file('compile-native-files.cmake',
+                                              binding, "")
+        assert 0 == rolt.compare_binding_file('AssemblyInfo.cs.in',
+                                              binding, "")
 
 
 @pytest.mark.parametrize('name, class_name', [
@@ -181,8 +201,8 @@ def test_cmake(name, class_name):
     :param class_name:   e.g. 'libsedml' or 'libcombine'
     """
     gv.reset()
-    filename = test_functions.set_up_test(name, class_name, "cmake")
-    rolt.generate_cmake(filename, "cmake")
+    xml_filename = test_functions.set_up_test(name, class_name, "cmake")
+    rolt.generate_cmake(xml_filename, "cmake")
     assert 0 == rolt.compare_cmake_file("")
     assert 0 == rolt.compare_cmake_file('src')
     assert 0 == rolt.compare_cmake_file('src/bindings')
@@ -201,8 +221,8 @@ def test_global(name, class_name, test_case):
     :param class_name: test class e.g. 'libsedml'
     :param test_case: brief description of test, e.g. 'global files'
     """
-    filename = test_functions.set_up_test(name, class_name, test_case)
-    rolt.generate_global(filename)
+    xml_filename = test_functions.set_up_test(name, class_name, test_case)
+    rolt.generate_global(xml_filename)
     assert 0 == rolt.compare_code_txt('VERSION')
     assert 0 == rolt.compare_code_txt('README', '.md')
 
@@ -215,11 +235,12 @@ def test_forward(name, class_name, test_case):
     Based on old run_forward() function.
     Run 'forward declaration' tests.
 
-    :param name: stub of XML filename, e.g. 'test_sedml' for file test_sedml.xml.
+    :param name: stub of XML filename, e.g. 'test_sedml'
+                 for file test_sedml.xml.
     :param class_name: test class, e.g. 'sedmlfwd'
     :param test_case: brief test description, e.g. 'forward declarations'
     """
     gv.reset()
-    filename = test_functions.set_up_test(name, class_name, test_case)
-    rolt.generate_forward(filename)
+    xml_filename = test_functions.set_up_test(name, class_name, test_case)
+    rolt.generate_forward(xml_filename)
     assert 0 == rolt.compare_code_headers(class_name)
