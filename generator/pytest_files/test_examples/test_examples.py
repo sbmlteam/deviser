daf3d2b60a9154e3536ac2de53aabfdc11c01d60
Matthew Gillman <m.gillman@ucl.ac.uk>
2021-04-22 10:28:43 +0100

    Initial commit of test_examples files.

---
+++ generator/pytest_files/test_examples/test_examples.py
@@ -0,0 +1,10 @@
+# Matthew S. Gillman, UCL, 22nd April 2021.
+import os
+import pytest
+
+from ...tests import test_functions
+from ...tests.test_examples import run_examples_tests as ret
+from ...util import global_variables as gv
+
+def test_me():
+    assert 1 == 1
