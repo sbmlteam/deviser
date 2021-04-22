a8ca59c08eb3ed9aa96f1e4416bbeda66832b06e
Matthew Gillman <m.gillman@ucl.ac.uk>
2021-04-22 12:56:36 +0100

    Fixed flake8 complaints in test_cpp.py.

--- generator/pytest_files/test_cpp/test_cpp.py
+++ generator/pytest_files/test_cpp/test_cpp.py
@@ -26,12 +26,16 @@ from ...tests.test_cpp_code import run_cpp_tests as rct  # For now.
     ('test_att', 0, 'MyTestClass', '', 'all types of attributes'),
     ('test_att', 3, 'MyRequiredClass', '', 'all types attributes required'),
     ('test_att', 4, 'ArrayChild', '', 'child elements and arrays'),
-    ('test_att', 5, 'Container', '', 'a listOf child that uses listOfFoo as the name'),
-    ('qual', 5, 'FunctionTerm', 'ListOfFunctionTerms', 'an element on ListOf'),
+    ('test_att', 5, 'Container', '',
+     'a listOf child that uses listOfFoo as the name'),
+    ('qual', 5, 'FunctionTerm', 'ListOfFunctionTerms',
+     'an element on ListOf'),
     ('qual', 3, 'Output', 'ListOfOutputs', 'simple class'),
-    ('qual', 1, 'Transition', 'ListOfTransitions', 'class with child list of elements'),
+    ('qual', 1, 'Transition', 'ListOfTransitions',
+     'class with child list of elements'),
     ('distrib', 2, 'UncertaintyOld', '', 'class with other child'),
-    ('spatial', 7, 'CoordinateComponent', 'ListOfCoordinateComponents', 'class with same child element diff name'),
+    ('spatial', 7, 'CoordinateComponent', 'ListOfCoordinateComponents',
+     'class with same child element diff name'),
     ('spatial', 15, 'CSGeometry', '', 'abstract'),
     ('spatial', 17, 'CSGNode', 'ListOfCSGNodes', 'abstract'),
     ('spatial', 16, 'CSGObject', '', 'abstract children'),
@@ -40,28 +44,37 @@ from ...tests.test_cpp_code import run_cpp_tests as rct  # For now.
     ('spatial', 9, 'SampledField', '', 'additional code files'),
     ('spatial', 18, 'CSGTransformation', '', 'abstract non base class'),
     ('spatial', 3, 'Boundary', '', 'overwrites element name'),
-    ('spatial', 13, 'ParametricGeometry', '', 'child element and child lo_element'),
-    ('spatial', 12, 'AnalyticVolume', 'ListOfAnalyticVolumes', 'class with math child'),
-    ('spatial', 5, 'GeometryDefinition', 'ListOfGeometryDefinitions', 'class with math child'),
+    ('spatial', 13, 'ParametricGeometry', '',
+     'child element and child lo_element'),
+    ('spatial', 12, 'AnalyticVolume', 'ListOfAnalyticVolumes',
+     'class with math child'),
+    ('spatial', 5, 'GeometryDefinition', 'ListOfGeometryDefinitions',
+     'class with math child'),
     ('spatial', 30, 'Geometry', '', 'class with math child'),
     ('spatial', 31, 'MixedGeometry', '', 'class with math child'),
-    ('spatial', 25, 'CSGSetOperator', '', 'contains list of that has abstracts that are abstract'),
+    ('spatial', 25, 'CSGSetOperator', '',
+     'contains list of that has abstracts that are abstract'),
     ('groups', 1, 'Member', 'ListOfMembers', 'list of with attribute'),
     ('groups', 0, 'Group', 'ListOfGroups', 'list of with attribute'),
     ('test_vers', 0, 'ClassOne', '', 'multiple versions'),
     ('test_vers', 2, 'BBB', '', 'multiple versions same child lo'),
-    ('fbc_v2', 1, 'Objective', 'ListOfObjectives', 'lo_element with attribute'),
+    ('fbc_v2', 1, 'Objective', 'ListOfObjectives',
+     'lo_element with attribute'),
     ('fbc_v2', 5, 'FbcAnd', '', 'inline_lo_element'),
-    ('fbc_v2', 7, 'Association', 'ListOfAssociations', 'lo_element with abstract children'),
+    ('fbc_v2', 7, 'Association', 'ListOfAssociations',
+     'lo_element with abstract children'),
     ('test_child', 0, 'MySEDClass', '', 'different language'),
     ('base_class', 0, 'MyBase', '', 'class with no given base class'),
-    ('base_class', 1, 'ClassTwo', 'ListOfClassTwos', 'class with lower case name'),
+    ('base_class', 1, 'ClassTwo', 'ListOfClassTwos',
+     'class with lower case name'),
     ('base_class', 2, 'ClassThree', '', 'class with enum lower case name'),
-    ('unknown_type', 0, 'UnknownType', '', 'class using an unknown attribute type'),
+    ('unknown_type', 0, 'UnknownType', '',
+     'class using an unknown attribute type'),
     ('nasty_lists', 0, 'Child', '', 'non uniform listOf name'),
     ('nasty_lists', 2, 'Parent', '', 'non uniform listOf name'),
     ('nasty_lists_alt1', 2, 'Parent', '', 'non uniform listOf name'),
-    ('lo_children', 1, 'ContainerX', '', 'variants of lo child elements and differing api/xml names'),
+    ('lo_children', 1, 'ContainerX', '',
+     'variants of lo child elements and differing api/xml names'),
     ('xml_names', 0, 'Fred', '', 'attribute xml names differ'),
     ('xml_names', 1, 'Other', '', 'element xml names differ'),
     ('add_copyright', 0, 'Copyright', '', 'additional copyright'),
@@ -69,9 +82,12 @@ from ...tests.test_cpp_code import run_cpp_tests as rct  # For now.
     ('core', 14, 'ModifierSpeciesReference', '', 'new attribute'),
     ('core', 22, 'Event', '', 'new element functions'),
     ('core', 11, 'Constraint', '', 'has XML Node child'),
-    ('new_distrib', 2, 'Distribution', '', 'abstract class with multiple abstract children'),
-    ('new_distrib', 0, 'DrawFromDistribution', '', 'abstract class with multiple abstract children'),
-    ('new_distrib', 6, 'DiscreteUnivariateDistribution', '', 'abstract class with multiple abstract children'),
+    ('new_distrib', 2, 'Distribution', '',
+     'abstract class with multiple abstract children'),
+    ('new_distrib', 0, 'DrawFromDistribution', '',
+     'abstract class with multiple abstract children'),
+    ('new_distrib', 6, 'DiscreteUnivariateDistribution', '',
+     'abstract class with multiple abstract children'),
     ('new_distrib', 15, 'BernoulliDistribution', '', 'concrete class'),
     ('new_distrib', 14, 'Category', 'ListOfCategories', 'concrete class'),
     ('new_distrib', 13, 'CategoricalDistribution', '', 'concrete class'),
@@ -79,15 +95,20 @@ from ...tests.test_cpp_code import run_cpp_tests as rct  # For now.
     ('new_distrib', 23, 'ExponentialDistribution', '', 'concrete class'),
     ('new_distrib', 17, 'BinomialDistribution', '', 'concrete class'),
     ('new_distrib', 21, 'Uncertainty', '', 'concrete class'),
-    ('new_distrib', 11, 'ExternalParameter', '', 'class contains a listOf itself'),
-    ('test_core_vers_multipkg', 0, 'ClassOneTwo', '', 'core version and package version not 1'),
+    ('new_distrib', 11, 'ExternalParameter', '',
+     'class contains a listOf itself'),
+    ('test_core_vers_multipkg', 0, 'ClassOneTwo', '',
+     'core version and package version not 1'),
     ('twoAtOnce', 1, 'ClassWithRequiredID', '', 'concrete class'),
-    ('twoAtOnce', 3, 'MultipleChild', '', 'multiple versions with child elements'),
+    ('twoAtOnce', 3, 'MultipleChild', '',
+     'multiple versions with child elements'),
 
     # leave out as need to work on automatically adding prefix:
     # ('new_distrib_test', 36, 'DistribUncertainty', '', 'concrete class'),
-    # ('new_distrib_test', 16, 'DistribBernoulliDistribution', '', 'concrete class'),
-    # ('new_distrib_test', 14, 'DistribCategoricalDistribution', '', 'concrete class'),
+    # ('new_distrib_test', 16, 'DistribBernoulliDistribution', '',
+    #    'concrete class'),
+    # ('new_distrib_test', 14, 'DistribCategoricalDistribution', '',
+    #    'concrete class'),
 
     ('render', 5, 'GradientStop', '', 'hyphenated attribute'),
     ('render', 12, 'Polygon', '', 'list of with different types elements'),
@@ -99,7 +120,8 @@ from ...tests.test_cpp_code import run_cpp_tests as rct  # For now.
     ('sbgn', 3, 'Map', '', 'list of with different types elements'),
     ('sbgn', 7, 'Arc', '', 'list of with different types elements'),
     ('sbgn', 2, 'Label', '', 'element with different xml name'),
-    ('sbgn', 0, 'Point', 'SbgnListOfPoints', 'contains list of itself but with other listof used elsewhere'),
+    ('sbgn', 0, 'Point', 'SbgnListOfPoints',
+     'contains list of itself but with other listof used elsewhere'),
 
     # To be sorted:
     # ('arrays', 1, 'Index', 'ListOfIndices', 'list of with attribute'),
@@ -120,9 +142,12 @@ def test_cpp(name, num, class_name, list_of, test_case):
     ('test_vers', 'VersExtension', 'multi version extension file', 0),
     ('base_class', 'TestExtension', 'default typecodes extension file', 0),
     ('test_core_vers', 'CoreversExtension', 'core version extension file', 0),
-    ('test_core_vers_pkg', 'CoreversPkgExtension', 'core version and package version not 1 extension file', 0),
-    ('test_core_vers_multipkg', 'CoreversMultiPkgExtension', 'multiple core version and package version not 1 extension file', 0),
-    ('twoAtOnce', 'TwoatonceExtension', 'basic extension file with SBasePlugin', 0),
+    ('test_core_vers_pkg', 'CoreversPkgExtension',
+     'core version and package version not 1 extension file', 0),
+    ('test_core_vers_multipkg', 'CoreversMultiPkgExtension',
+     'multiple core version and package version not 1 extension file', 0),
+    ('twoAtOnce', 'TwoatonceExtension',
+     'basic extension file with SBasePlugin', 0),
     ('multi', 'MultiExtension', 'basic extension file', 0),
 
     # not yet sorted:
@@ -149,10 +174,12 @@ def test_cpp_ext(name, class_name, test_case, test):
     ('test_vers', 'VersModelPlugin', 'versions of plugins - attributes', 0),
     ('test_vers', 'VersSpeciesPlugin', 'versions of plugins - elements', 1),
     ('fbc_v2', 'FbcModelPlugin', 'plugin with attributes', 0),
-    ('test_core_vers_multipkg', 'CoreversMultiPkgModelPlugin', 'versions of plugins - elements', 0),
+    ('test_core_vers_multipkg', 'CoreversMultiPkgModelPlugin',
+     'versions of plugins - elements', 0),
     ('twoAtOnce', 'TwoatonceSBasePlugin', 'an SBase plugin', 0),
     ('plugin_id', 'PluginidSBasePlugin', 'an SBase plugin', 0),
-    ('comp', 'CompSBMLDocumentPlugin', 'document plugin with additional items', 3),
+    ('comp', 'CompSBMLDocumentPlugin',
+     'document plugin with additional items', 3),
 ])
 def test_cpp_plugin(name, class_name, test_case, num):
     """
@@ -162,7 +189,7 @@ def test_cpp_plugin(name, class_name, test_case, num):
     assert rct.run_plug_test(name, class_name, test_case, num) == 0
 
 
-@pytest.mark.parametrize("name, class_name, test_case, generate_error_files", [
+@pytest.mark.parametrize("name, class_name, test_case, gen_error_files", [
     ('test_att', 'TestSBMLError', 'error enumeration', True),
     ('qual', 'QualSBMLError', 'error enumeration', True),
     ('qual', 'QualConsistencyValidator', 'validator', False),
@@ -177,10 +204,14 @@ def test_cpp_plugin(name, class_name, test_case, num):
     # element="ThingA, ThingB"
     # element=" ThingA, ThingB"
     # element="ThingA,ThingB"
-    ('test_sidrefs_1', 'RefsSBMLError', 'sidref with multiple targets - diff spacing', True),
-    ('test_sidrefs_2', 'RefsSBMLError', 'sidref with multiple targets - diff spacing', True),
-    ('test_sidrefs_3', 'RefsSBMLError', 'sidref with multiple targets - diff spacing', True),
-    ('test_sidrefs_4', 'RefsSBMLError', 'sidref with multiple targets - diff spacing', True),
+    ('test_sidrefs_1', 'RefsSBMLError',
+     'sidref with multiple targets - diff spacing', True),
+    ('test_sidrefs_2', 'RefsSBMLError',
+     'sidref with multiple targets - diff spacing', True),
+    ('test_sidrefs_3', 'RefsSBMLError',
+     'sidref with multiple targets - diff spacing', True),
+    ('test_sidrefs_4', 'RefsSBMLError',
+     'sidref with multiple targets - diff spacing', True),
 
     ('test_lists', 'FooSBMLError', 'error enumeration', True),
     ('new_distrib', 'DistribSBMLError', 'error enumeration', True),
@@ -192,12 +223,13 @@ def test_cpp_plugin(name, class_name, test_case, num):
 
     # ('arrays', 'ArraysValidator', 'validator', False),
 ])
-def test_cpp_valid(name, class_name, test_case, generate_error_files):
+def test_cpp_valid(name, class_name, test_case, gen_error_files):
     """
     This is just a wrapper function - see run_cpp_test.py for the
     documentation of the function it calls here.
     """
-    assert rct.run_valid_test(name, class_name, test_case, generate_error_files) == 0
+    assert rct.run_valid_test(name, class_name, test_case,
+                              gen_error_files) == 0
 
 
 @pytest.mark.parametrize("name, class_name, test_case", [
