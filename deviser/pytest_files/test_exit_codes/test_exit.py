d53824bb0d18a9b5cd478f43cda95bcf217a4547
Sarah <s.keating@ucl.ac.uk>
2021-07-24 11:44:31 +0100

    testing with and without outdir declared

--- deviser/pytest_files/test_exit_codes/test_exit.py
+++ deviser/pytest_files/test_exit_codes/test_exit.py
@@ -1,6 +1,8 @@
 # Trying out pytest with Deviser generator tests
 # Matthew Gillman, UCL, 16th December 2020
 import os
+import shutil
+
 import pytest
 
 from ...pytest_files import functions
@@ -8,27 +10,51 @@ from ...util import global_variables as gv
 from deviser.__main__ import run_generation
 
 
-@pytest.mark.parametrize("name, flag, expected_return", [
-    ('non-existent', '-g', 'failed to read file'),
-    ('test_child', '-g', 'success'),
-    ('test_child', 'missing', 'incorrect number function arguments'),
-    ('test_child', 'added', 'incorrect number function arguments'),
-    ('test_child', 'wrong', 'invalid function arguments'),
-    ('test_child', 'outdirwrong', 'invalid function arguments'),
-    # ('test_child', '-l', 'success'),  # Apparently problematic before?
-    ('invalid', '-g', 'parsing error'),
-    ('invalid', '-l', 'parsing error'),
-    ('unknown_type', '-g', 'unknown type used'),
-    ('unknown_error', '-g', 'missing required information'),
-    ('bad_lo_element', '-g', 'unknown type used'),
-    ('bad_concretes', '-g', 'missing required information'),
+def setup():
+    # Set up the environment.
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(this_dir)
+    os.chdir('..')
+    os.mkdir('testingout')
+    os.chdir(this_dir)
+
+
+def teardown():
+    # delete temp dir
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    temp_dir = os.path.join(this_dir, 'temp')
+    if os.path.isdir(temp_dir):
+        shutil.rmtree(temp_dir)
+    os.chdir(this_dir)
+    os.chdir('..')
+    if os.path.isdir('testingout'):
+        shutil.rmtree('testingout')
+    os.chdir(this_dir)
+
+
+@pytest.mark.parametrize("name, flag, outdir, expected_return", [
+    ('non-existent', '-g', 'None', 'failed to read file'),
+    ('test_child', '-g', 'None', 'success'),
+    ('test_child', 'missing', 'None',  'incorrect number function arguments'),
+    ('test_child', 'added', 'None', 'incorrect number function arguments'),
+    ('test_child', 'wrong', 'None', 'invalid function arguments'),
+    ('test_child', '-g', 'outdir', 'invalid function arguments'),
+    ('test_child', '-g',  '..\\testingout', 'success'),
+    # ('test_child', '-l', 'None', 'success'),  # Apparently problematic before?
+    ('invalid', '-g', 'None', 'parsing error'),
+    ('invalid', '-l', 'None', 'parsing error'),
+    ('unknown_type', '-g', 'None',  'unknown type used'),
+    ('unknown_error', '-g', 'None',  'missing required information'),
+    ('bad_lo_element', '-g', 'None',  'unknown type used'),
+    ('bad_concretes', '-g', 'None',  'missing required information'),
 ])
-def test_deviser(name, flag, expected_return):
+def test_deviser(name, flag, outdir, expected_return):
     """
     General function for running command-line Deviser tests.
 
     :param name: general name of test
     :param flag: a command-line flag
+    :param outdir: optional output dir
     :param expected_return: what we expect the test to return.
     :return: 0 on success, 1 on failure
     """
@@ -44,6 +70,8 @@ def test_deviser(name, flag, expected_return):
         args.append('deviser')
         args.append(flag)
         args.append(filename)
+        if outdir != 'None':
+            args.append(outdir)
     elif flag == 'missing':
         args.append('deviser')
         args.append(filename)
@@ -53,13 +81,7 @@ def test_deviser(name, flag, expected_return):
         args.append(filename)
         args.append('outdir')
         args.append('extra')
-    elif flag == 'outdirwrong':
-        args.append('deviser')
-        args.append('-g')
-        args.append(filename)
-        args.append('outdir')
     else:
         raise Exception
     run_generation(args)
-
     assert gv.code_returned == expected_return_number
